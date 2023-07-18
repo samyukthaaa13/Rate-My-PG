6bc8efceb7cab94f579cbc3de5d69b8ec6f1f436
samyukthaaa13 <139615251+samyukthaaa13@users.noreply.github.com>
2023-07-18 18:50:16 +0000

    Added new route for resources

--- app.py
+++ app.py
@@ -6,5 +6,11 @@ app = Flask(__name__)
 def hello_world():
     return render_template('home.html')
 
+# Route for the "resources.html" page
+@app.route('/resources.html')
+def resources():
+    return render_template('resources.html')
+
+
 if __name__=='__main__':
   app.run(host='0.0.0.0',debug=True)
